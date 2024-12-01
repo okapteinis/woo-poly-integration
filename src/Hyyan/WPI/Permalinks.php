966014e56fe2438f3b6d620e464f31c0d40d132a
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-01 19:37:33 +0200

    Update Permalinks.php

--- src/Hyyan/WPI/Permalinks.php
+++ src/Hyyan/WPI/Permalinks.php
@@ -1,59 +1,39 @@
 <?php
 
-/**
- * This file is part of the hyyan/woo-poly-integration plugin.
- * (c) Hyyan Abo Fakher <hyyanaf@gmail.com>.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Hyyan\WPI;
 
-/**
- * Permalinks.
- *
- * @author Hyyan Abo Fakher <hyyanaf@gmail.com>
- */
 class Permalinks
 {
-    const PRODUCT_BASE = '/product';
-    const PRODUCT_CATEGORY_BASE = 'product-category';
-    const PRODUCT_TAG_BASE = 'product-tags';
+    public const PRODUCT_BASE = '/product';
+    public const PRODUCT_CATEGORY_BASE = 'product-category';
+    public const PRODUCT_TAG_BASE = 'product-tags';
 
-    /**
-     * Construct object.
-     */
     public function __construct()
     {
-        add_action('init', array($this, 'setDefaultPermalinks'), 11);
+        add_action('init', [$this, 'setDefaultPermalinks'], 11);
     }
 
-    /**
-     * Set default permalinks.
-     *
-     * Setup the right permalinks to work with polylang if used permalinks is the
-     * default woocommerce permalinks
-     * (This was getting called too often, also a situation occurred where these got set to boolean...)
-     */
-    public function setDefaultPermalinks()
+    public function setDefaultPermalinks(): void
     {
-        $permalinks = get_option('woocommerce_permalinks');
-        $was_set=false;
-        if (! isset($permalinks['category_base']) || (is_bool($permalinks['category_base']))) {
+        $permalinks = get_option('woocommerce_permalinks') ?: [];
+        $wasSet = false;
+
+        if (!isset($permalinks['category_base']) || is_bool($permalinks['category_base'])) {
             $permalinks['category_base'] = self::PRODUCT_CATEGORY_BASE;
-            $was_set=true;
-        }
-        if (! isset($permalinks['tag_base']) || (is_bool($permalinks['tag_base']))) {
-            $permalinks['tag_base'] =  self::PRODUCT_TAG_BASE;
-            $was_set=true;
-        }
-        if (! isset($permalinks['product_base']) || (is_bool($permalinks['product_base']))) {
-            $permalinks['product_base'] = self::PRODUCT_BASE;
-            $was_set=true;
+            $wasSet = true;
         }
 
-        if ($was_set) {
+        if (!isset($permalinks['tag_base']) || is_bool($permalinks['tag_base'])) {
+            $permalinks['tag_base'] = self::PRODUCT_TAG_BASE;
+            $wasSet = true;
+        }
+
+        if (!isset($permalinks['product_base']) || is_bool($permalinks['product_base'])) {
+            $permalinks['product_base'] = self::PRODUCT_BASE;
+            $wasSet = true;
+        }
+
+        if ($wasSet) {
             update_option('woocommerce_permalinks', $permalinks);
         }
     }

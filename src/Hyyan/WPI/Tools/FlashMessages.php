0c1386130fb1f1ffed09dc5a25b71dc3d987ccb3
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-01 22:59:03 +0200

    Update FlashMessages.php

--- src/Hyyan/WPI/Tools/FlashMessages.php
+++ src/Hyyan/WPI/Tools/FlashMessages.php
@@ -1,141 +1,88 @@
 <?php
 
-/**
- * This file is part of the hyyan/woo-poly-integration plugin.
- * (c) Hyyan Abo Fakher <hyyanaf@gmail.com>.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Hyyan\WPI\Tools;
 
-/**
- * FlashMessages.
- *
- * @author Hyyan Abo Fakher <hyyanaf@gmail.com>
- */
 final class FlashMessages
 {
-    /**
-     * Register the falsh messages.
-     */
-    public static function register()
+    private const OPTION_NAME = 'hyyan-wpi-flash-messages';
+
+    public static function register(): void
     {
-        add_action('admin_notices', array(
-            __CLASS__, 'display',
-        ));
+        add_action('admin_notices', [__CLASS__, 'display']);
     }
 
-    /**
-     * Queue flash message.
-     *
-     * @param string $id      message id
-     * @param string $message message content
-     * @param array  $classes array of classes to used for this message wrapper
-     * @param bool   $persist should persist the message between sessions
-     */
-    public static function add($id, $message, array $classes = array('updated'), $persist = false)
-    {
-        $messages = static::getMessages();
-        $data = array(
+    public static function add(
+        string $id,
+        string $message,
+        array $classes = ['updated'],
+        bool $persist = false
+    ): void {
+        $messages = self::getMessages();
+        $data = [
             'id' => $id,
             'message' => $message,
             'classes' => $classes,
             'persist' => $persist,
-        );
+        ];
 
-        /* Add the new message data */
-        if (isset($messages[$id])) {
-            $messages[$id] = array_replace_recursive($messages[$id], $data);
-        } else {
-            $messages[$id] = $data;
-        }
+        $messages[$id] = isset($messages[$id]) ? 
+            array_replace_recursive($messages[$id], $data) : 
+            $data;
 
-        update_option(static::getOptionName(), $messages);
+        update_option(self::OPTION_NAME, $messages);
     }
 
-    /**
-     * Remove message by its id.
-     *
-     * @param string $id message id
-     *
-     * @return bool true if removed , false otherwise
-     */
-    public static function remove($id)
+    public static function remove(string $id): bool
     {
-        $messages = static::getMessages();
-        if (isset($messages[$id])) {
-            unset($messages[$id]);
-            update_option(static::getOptionName(), $messages);
+        $messages = self::getMessages();
+        if (!isset($messages[$id])) {
+            return false;
+        }
 
+        unset($messages[$id]);
+        update_option(self::OPTION_NAME, $messages);
+        return true;
+    }
+
+    public static function display(): void
+    {
+        $messages = self::getMessages();
+
+        foreach ($messages as $id => $message) {
+            if (!self::shouldDisplayMessage($message)) {
+                continue;
+            }
+
+            $messages[$id]['displayed'] = !$message['persist'];
+            
+            $message['classes'][] = 'is-dismissible notice';
+            printf(
+                '<div class="%s"><p>%s</p></div>',
+                implode(' ', $message['classes']),
+                $message['message']
+            );
+        }
+
+        update_option(self::OPTION_NAME, $messages);
+    }
+
+    public static function clearMessages(): void
+    {
+        delete_option(self::OPTION_NAME);
+    }
+
+    private static function getMessages(): array
+    {
+        $messages = get_option(self::OPTION_NAME, []);
+        return is_array($messages) ? $messages : [];
+    }
+
+    private static function shouldDisplayMessage(array $message): bool
+    {
+        if (!isset($message['displayed'])) {
             return true;
         }
 
-        return false;
-    }
-
-    /**
-     * Display all flash messages.
-     */
-    public static function display()
-    {
-        $messages = static::getMessages();
-
-        foreach ($messages as $id => $message) {
-            $display = true;
-            if (!isset($message['displayed'])) {
-                $display = true;
-            } elseif ($message['persist'] === false && $message['displayed'] === true) {
-                $display = false;
-            }
-
-            $messages[$id]['displayed'] = (true === $message['persist']) ?
-                    false : true;
-
-            if (true === $display || !@$message['displayed']) {
-                $message['classes'][] = 'is-dismissible notice';
-                $classesString = implode(' ', $message['classes']);
-                printf(
-                        '<div class="%s"><p>%s</p></div>', $classesString, $message['message']
-                );
-            }
-        }
-
-        update_option(static::getOptionName(), $messages);
-    }
-
-    /**
-     * Clear all messages.
-     */
-    public static function clearMessages()
-    {
-        delete_option(static::getOptionName());
-    }
-
-    /**
-     * Get option name used to save flash messages to database.
-     *
-     * @return string
-     */
-    public static function getOptionName()
-    {
-        return 'hyyan-wpi-flash-messages';
-    }
-
-    /**
-     * Get messages.
-     *
-     * Get flash messages array
-     *
-     * @return array
-     */
-    private static function getMessages()
-    {
-        $messages = get_option( static::getOptionName(), array() );
-        if ( ! is_array( $messages ) ) {
-            $messages = array();
-        }
-        return $messages;
+        return $message['persist'] || !$message['displayed'];
     }
 }

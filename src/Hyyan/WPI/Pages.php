a8e4add27c436ea2e5ea46fccead21c343da6989
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-01 19:33:30 +0200

    Update Pages.php

--- src/Hyyan/WPI/Pages.php
+++ src/Hyyan/WPI/Pages.php
@@ -1,145 +1,83 @@
 <?php
 
-/**
- * This file is part of the hyyan/woo-poly-integration plugin.
- * (c) Hyyan Abo Fakher <hyyanaf@gmail.com>.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Hyyan\WPI;
 
-/**
- * Pages.
- *
- * Handle page translations
- *
- * @author Hyyan Abo Fakher <hyyanaf@gmail.com>
- */
+use WP;
+
 class Pages
 {
-
-    /**
-     * Construct object.
-     */
     public function __construct()
     {
-        $method = array($this, 'getPostTranslationID');
-        $pages = apply_filters(HooksInterface::PAGES_LIST, array(
+        $method = [$this, 'getPostTranslationID'];
+        $pages = apply_filters(HooksInterface::PAGES_LIST, [
             'shop',
             'cart',
             'checkout',
             'terms',
             'myaccount',
-        ));
+        ]);
 
         foreach ($pages as $page) {
             add_filter(sprintf('woocommerce_get_%s_page_id', $page), $method);
             add_filter(sprintf('option_woocommerce_%s_page_id', $page), $method);
         }
 
-        /* To generate the correct url for shop page */
-        add_filter(
-                'pll_get_archive_url', array($this, 'translateShopUrl'), 10, 2
-        );
+        add_filter('pll_get_archive_url', [$this, 'translateShopUrl'], 10, 2);
 
         if (!is_admin()) {
-
-            /* To get product from current language in the shop page */
-            add_filter('parse_request', array($this, 'correctShopPage'));
+            add_filter('parse_request', [$this, 'correctShopPage']);
         }
 
-        add_filter(
-                'woocommerce_shortcode_products_query', array($this, 'addShortcodeLanguageFilter'), 10, 2
-        );
-		add_filter( 'shortcode_atts_product_categories', array( $this, 'addShortcodeLanguageFilterCategories' ), 10, 4 );
+        add_filter('woocommerce_shortcode_products_query', [$this, 'addShortcodeLanguageFilter'], 10, 2);
+        add_filter('shortcode_atts_product_categories', [$this, 'addShortcodeLanguageFilterCategories'], 10, 4);
     }
 
-    /**
-     * Get the id of translated post.
-     *
-     * @param int $id the post to get translation id for
-     *
-     * @return int
-     */
-    public function getPostTranslationID($id)
+    public function getPostTranslationID(int $id): int
     {
         if (!function_exists('pll_get_post')) {
             return $id;
         }
 
-        $translatedID = pll_get_post($id);
-
-        if ($translatedID) {
-            return $translatedID;
-        }
-
-        return $id;
+        return pll_get_post($id) ?: $id;
     }
 
-    /**
-     * Correct the shop page to display products from currrent language only.
-     *
-     * @param \WP $wp wordpress instance
-     *
-     * @return bool false if the current language is the same as default
-     *              language or if the "pagename" var is empty
-     */
-    public function correctShopPage(\WP $wp)
+    public function correctShopPage(WP $wp): bool
     {
-        global $polylang;
-
         $shopID = wc_get_page_id('shop');
-        $shopOnFront = ('page' === get_option('show_on_front')) && in_array(
-                        get_option('page_on_front'), PLL()->model->post->get_translations(
-                                $shopID
-        ));
+        $shopOnFront = ('page' === get_option('show_on_front')) && 
+            in_array(
+                get_option('page_on_front'),
+                PLL()->model->post->get_translations($shopID),
+                true
+            );
 
-        $vars = array('pagename', 'page', 'name');
+        $vars = ['pagename', 'page', 'name'];
         foreach ($vars as $var) {
             if (isset($wp->query_vars[$var])) {
                 $shopOnFront = false;
                 break;
             }
         }
-        if (!$shopOnFront) {
-            if (!empty($wp->query_vars['pagename'])) {
-                $shopPage = get_post($shopID);
 
-                /* Explode by / for children page */
-                $page = explode('/', $wp->query_vars['pagename']);
+        if (!$shopOnFront && !empty($wp->query_vars['pagename'])) {
+            $shopPage = get_post($shopID);
+            $page = explode('/', $wp->query_vars['pagename']);
 
-                if (
-                        isset($shopPage->post_name) &&
-                        $shopPage->post_name == $page[count($page) - 1]
-                ) {
-                    unset($wp->query_vars['page']);
-                    unset($wp->query_vars['pagename']);
-                    $wp->query_vars['post_type'] = 'product';
-                }
+            if (isset($shopPage->post_name) && 
+                $shopPage->post_name === $page[count($page) - 1]
+            ) {
+                unset($wp->query_vars['page'], $wp->query_vars['pagename']);
+                $wp->query_vars['post_type'] = 'product';
             }
         }
+
+        return false;
     }
 
-    /**
-     * Translate the shop page name in the given shop url.
-     *
-     * @param string $url      complete url
-     * @param string $language the current language
-     *
-     * @return string translated url
-     */
-    public function translateShopUrl($url, $language)
+    public function translateShopUrl(string $url, string $language): string
     {
-        $result = $url;
-
-        if (!is_post_type_archive('product')) {
-            return $result;
-        }
-
-        if ( is_front_page() ) {
-            return $result;
+        if (!is_post_type_archive('product') || is_front_page()) {
+            return $url;
         }
 
         $shopPageID = get_option('woocommerce_shop_page_id');
@@ -149,68 +87,51 @@ class Pages
             $shopPageTranslatedID = pll_get_post($shopPageID, $language);
             $shopPageTranslation = get_post($shopPageTranslatedID);
 
-            if ($shopPageTranslation) {
-		        		$slug = $shopPage->post_name;
-        				if ( $slug != $shopPageTranslation->post_name ) {
-        					$result = substr_replace( $url, $shopPageTranslation->post_name, strrpos( $url, $slug ), strlen( $slug ) );
-        				}
+            if ($shopPageTranslation && $shopPage->post_name !== $shopPageTranslation->post_name) {
+                return substr_replace(
+                    $url,
+                    $shopPageTranslation->post_name,
+                    strrpos($url, $shopPage->post_name),
+                    strlen($shopPage->post_name)
+                );
             }
         }
 
-        return $result;
+        return $url;
     }
 
-    /**
-     * Add Shortcode Language Filter
-     *
-     * Fix shortcodes to include language filter.
-     *
-     * @param array $query_args
-     * @param array $atts
-     * @param string $loop_name  --  not provided by some shortcodes
-     *
-     * @return string modified form
-     */
-    public function addShortcodeLanguageFilter($query_args, $atts)
+    public function addShortcodeLanguageFilter(array $query_args, array $atts): array
     {
-        if (isset($atts['ids']) && strlen($atts['ids'])) {
-            $ids = explode(',', $atts['ids']);
-            $transIds = array();
-            foreach ($ids as $id) {
-                array_push($transIds, pll_get_post($id));
-            }
-
-            $atts['ids'] = implode($transIds, ',');
+        if (!empty($atts['ids'])) {
+            $transIds = array_map('pll_get_post', explode(',', $atts['ids']));
             $query_args['post__in'] = $transIds;
-            if ( isset( $query_args[ 'p' ] ) && ( $query_args[ 'p' ] != $transIds) ) {
-                unset( $query_args[ 'p' ] );
+            
+            if (isset($query_args['p']) && $query_args['p'] !== $transIds) {
+                unset($query_args['p']);
             }
+            
+            $atts['ids'] = implode(',', $transIds);
         } else {
-            $query_args['lang'] = isset($query_args['lang']) ?
-                    $query_args['lang'] : pll_current_language();
+            $query_args['lang'] = $query_args['lang'] ?? pll_current_language();
         }
         
         return $query_args;
     }
 
-	public function addShortcodeLanguageFilterCategories( $out, $pairs, $atts, $shortcode ) {
+    public function addShortcodeLanguageFilterCategories(array $out, array $pairs, array $atts, string $shortcode): array
+    {
+        if (!empty($atts['ids'])) {
+            $transIds = array_map('pll_get_term', explode(',', $atts['ids']));
+            $out['ids'] = implode(',', $transIds);
+        }
 
-		if ( isset( $atts[ 'ids' ] ) && strlen( $atts[ 'ids' ] ) ) {
-			$ids		 = explode( ',', $atts[ 'ids' ] );
-			$transIds	 = array();
-			foreach ( $ids as $id ) {
-				array_push( $transIds, pll_get_term( $id ) );
-			}
-			$out[ 'ids' ] = implode( $transIds, ',' );
-		}
+        if (!empty($atts['parent'])) {
+            $transParent = pll_get_term($atts['parent']);
+            if ($transParent) {
+                $out['parent'] = $transParent;
+            }
+        }
 
-		if ( isset( $atts[ 'parent' ] ) && strlen( $atts[ 'parent' ] ) ) {
-			$transParent = pll_get_term( $atts[ 'parent' ] );
-			if ( $transParent ) {
-				$out[ 'parent' ] = $transParent;
-			}
-		}
-		return $out;
-	}
-
+        return $out;
+    }
 }

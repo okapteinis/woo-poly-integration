9bc345e8e89d9b5d2f9f50580a9f36fb21304bee
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-01 19:29:49 +0200

    Update Media.php

--- src/Hyyan/WPI/Media.php
+++ src/Hyyan/WPI/Media.php
@@ -1,67 +1,31 @@
 <?php
 
-/**
- * This file is part of the hyyan/woo-poly-integration plugin.
- * (c) Hyyan Abo Fakher <hyyanaf@gmail.com>.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Hyyan\WPI;
 
-/**
- * Media.
- *
- * Handle products media translation
- *
- * @author Hyyan Abo Fakher <hyyanaf@gmail.com>
- */
 class Media
 {
-
-    /**
-     * Construct object.
-     */
     public function __construct()
     {
         if (static::isMediaTranslationEnabled()) {
             add_filter(
                 'woocommerce_product_get_gallery_image_ids',
-                 array($this, 'translateGallery')
+                [$this, 'translateGallery']
             );
         }
     }
 
-    /**
-     * Check if media translation is enable in polylang settings.
-     *
-     * @return bool true if enabled , false otherwise
-     */
-    public static function isMediaTranslationEnabled()
+    public static function isMediaTranslationEnabled(): bool
     {
         $options = get_option('polylang');
-
-        return $options['media_support'];
+        return (bool) ($options['media_support'] ?? false);
     }
 
-    /**
-     * Translate product gallery.
-     *
-     * @param array $IDS current attachment IDS
-     *
-     * @return array translated attachment IDS
-     */
-    public function translateGallery(array $IDS)
+    public function translateGallery(array $IDS): array
     {
-        $translations = array();
+        $translations = [];
         foreach ($IDS as $ID) {
             $tr = pll_get_post($ID);
-            if ($tr) {
-                $translations [] = $tr;
-                continue;
-            }
-            $translations [] = $ID;
+            $translations[] = $tr ?: $ID;
         }
 
         return $translations;

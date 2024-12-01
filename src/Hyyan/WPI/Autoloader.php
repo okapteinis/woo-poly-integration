8eed49d5ca7877b087f0beda869c0a0783106981
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-01 19:01:20 +0200

    Update Autoloader.php

--- src/Hyyan/WPI/Autoloader.php
+++ src/Hyyan/WPI/Autoloader.php
@@ -1,62 +1,34 @@
 <?php
 
-/**
- * This file is part of the hyyan/woo-poly-integration plugin.
- * (c) Hyyan Abo Fakher <hyyanaf@gmail.com>.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Hyyan\WPI;
 
 /**
  * Plugin Namespace Autoloader.
- *
- * @author Hyyan Abo Fakher <hyyanaf@gmail.com>
  */
 final class Autoloader
 {
-    /**
-     * @var string
-     */
-    private $base;
+    private string $base;
 
     /**
-     * Construct the autoloader class.
-     *
-     * @param string $base the base path to use
-     *
-     * @throws \Exception when the autloader can not register itself
+     * @throws \Exception
      */
-    public function __construct($base)
+    public function __construct(string $base)
     {
         $this->base = $base;
-        spl_autoload_register(array($this, 'handle'), true, true);
+        spl_autoload_register([$this, 'handle'], true, true);
     }
 
-    /**
-     * Handle autoloading.
-     *
-     * @param string $className class or inteface name
-     *
-     * @return bool true if class or interface exists , false otherwise
-     */
-    public function handle($className)
+    public function handle(string $className): ?bool
     {
         if (stripos($className, "Hyyan\WPI") === false) {
-            return;
+            return null;
         }
 
-        $filename = $this->base.str_replace('\\', '/', $className).'.php';
+        $filename = $this->base . str_replace('\\', '/', $className) . '.php';
+        
         if (file_exists($filename)) {
             require_once $filename;
-            if (
-                    class_exists($className) ||
-                    interface_exists($className)
-            ) {
-                return true;
-            }
+            return class_exists($className) || interface_exists($className);
         }
 
         return false;

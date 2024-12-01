72d5be91df6e4dfc521bf1fa3add2e8d03c62344
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-01 20:02:01 +0200

    Update Settings.php

--- src/Hyyan/WPI/Admin/Settings.php
+++ src/Hyyan/WPI/Admin/Settings.php
@@ -1,116 +1,74 @@
 <?php
 
-/**
- * This file is part of the hyyan/woo-poly-integration plugin.
- * (c) Hyyan Abo Fakher <hyyanaf@gmail.com>.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Hyyan\WPI\Admin;
 
 use Hyyan\WPI\HooksInterface;
+use Hyyan\WPI\Plugin;
+use WeDevs_Settings_API;
 
-/**
- * Settings.
- *
- * Admin settings page
- *
- * @author Hyyan Abo Fakher <hyyanaf@gmail.com>
- */
-class Settings extends \WeDevs_Settings_API
+class Settings extends WeDevs_Settings_API
 {
-    /**
-     * Construct object.
-     */
     public function __construct()
     {
         parent::__construct();
-        add_action('admin_init', array($this, 'init'));
-        add_action('admin_menu', array($this, 'registerMenu'));
+        add_action('admin_init', [$this, 'init']);
+        add_action('admin_menu', [$this, 'registerMenu']);
 
         new Features();
         new MetasList();
     }
 
-    /**
-     * Initialize settings.
-     */
-    public function init()
+    public function init(): void
     {
-        /* Set the settings */
         $this->set_sections($this->getSections());
         $this->set_fields($this->getFields());
-
-        /* Initialize settings */
         $this->admin_init();
     }
 
-    /**
-     * Add plugin menu.
-     */
-    public function registerMenu()
+    public function registerMenu(): void
     {
-	  	if ( current_user_can( 'manage_options' ) ) {
+        if (!current_user_can('manage_options')) {
+            return;
+        }
+
         add_options_page(
-                __('Hyyan WooCommerce Polylang Integration', 'woo-poly-integration'), __('WooPoly', 'woo-poly-integration'), 'delete_posts', 'hyyan-wpi', array($this, 'outputPage')
+            __('Hyyan WooCommerce Polylang Integration', 'woo-poly-integration'),
+            __('WooPoly', 'woo-poly-integration'),
+            'delete_posts',
+            'hyyan-wpi',
+            [$this, 'outputPage']
         );
-      }
     }
 
-    /**
-     * Get sections.
-     *
-     * Get setting sections array to register
-     *
-     * @return array
-     */
-    public function getSections()
+    public function getSections(): array
     {
-        return apply_filters(HooksInterface::SETTINGS_SECTIONS_FILTER, array());
+        return apply_filters(HooksInterface::SETTINGS_SECTIONS_FILTER, []);
     }
 
-    /**
-     * Returns all the settings fields.
-     *
-     * @return array settings fields
-     */
-    public function getFields()
+    public function getFields(): array
     {
-        return apply_filters(HooksInterface::SETTINGS_FIELDS_FILTER, array());
+        return apply_filters(HooksInterface::SETTINGS_FIELDS_FILTER, []);
     }
 
-    /**
-     * Output page content.
-     */
-    public function outputPage()
+    public function outputPage(): void
     {
-        echo \Hyyan\WPI\Plugin::getView('admin', array(
+        echo Plugin::getView('admin', [
             'self' => $this,
-        ));
+        ]);
     }
 
-    /**
-     * Get the value of a settings field.
-     *
-     * @param string $option  settings field name
-     * @param string $section the section name this field belongs to
-     * @param string $default default text if it's not found
-     *
-     * @return mixed
-     */
-    public static function getOption($option, $section, $default = '')
+    public static function getOption(string $option, string $section, string $default = ''): mixed
     {
         $options = get_option($section);
 
-        if (!empty($options[$option])) {  // equivalent to: isset($options[$option]) && $options[$option]
+        if (!empty($options[$option])) {
             return $options[$option];
-        }   // when all settings are disabled
-        elseif (isset($options[$option])) {
-            return array();
-        } else {
-            return $default;
         }
+
+        if (isset($options[$option])) {
+            return [];
+        }
+
+        return $default;
     }
 }

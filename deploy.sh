229799e49d59194014d680075095a14952b98e2f
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-02 12:01:05 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
+
+set -e  # Exit on error
+
+# Clean build directory
 rm -rf build
 
-# prepare files to upload
+# Prepare files to upload
 rsync -avh . ./build --exclude-from '.exclude-list' --delete
 
-# change to build
+# Change to build directory
 cd build
 
 # Checkout the SVN repo
-svn co -q "http://svn.wp-plugins.org/woo-poly-integration" svn
-
-# Move out the trunk directory to a temp location
-#mv ./svn/trunk ./svn-trunk
+svn co -q "https://plugins.svn.wordpress.org/woo-poly-integration" svn
 
 # Create trunk directory
 mkdir -p svn/trunk
@@ -19,41 +20,14 @@ mkdir -p svn/trunk
 # Copy our new version of the plugin into trunk
 rsync -r -p ./* svn/trunk
 
-# # Copy all the .svn folders from the checked out copy of trunk to the new trunk.
-# # This is necessary as the Travis container runs Subversion 1.6 which has .svn dirs in every sub dir
-# cd svn/trunk/
-# TARGET=$(pwd)
-# cd ../../svn-trunk/
-
-# # Find all .svn dirs in sub dirs
-# SVN_DIRS=`find . -type d -iname .svn`
-
-# for SVN_DIR in $SVN_DIRS; do
-#     SOURCE_DIR=${SVN_DIR/.}
-#     TARGET_DIR=$TARGET${SOURCE_DIR/.svn}
-#     TARGET_SVN_DIR=$TARGET${SVN_DIR/.}
-#     if [ -d "$TARGET_DIR" ]; then
-#         # Copy the .svn directory to trunk dir
-#         cp -r $SVN_DIR $TARGET_SVN_DIR
-#     fi
-# done
-
-# # Back to builds dir
-# cd ../
-
-# Remove checked out dir
-#rm -fR svn-trunk
-
-# Add new version tag
-#mkdir -p ./svn/tags/$TRAVIS_TAG
-#rsync -r -p . svn/tags/$TRAVIS_TAG
-
 # Add new files to SVN
 svn stat svn | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
+
 # Remove deleted files from SVN
 svn stat svn | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
+
+# Show status
 svn stat svn
 
 # Commit to SVN
-svn ci --no-auth-cache --username hyyan --password $WP_ORG_PASSWORD svn -m "build version $TRAVIS_TAG"
-
+svn ci --no-auth-cache --username "$WP_ORG_USERNAME" --password "$WP_ORG_PASSWORD" svn -m "Update to version $TRAVIS_TAG with PHP 8.4 support"

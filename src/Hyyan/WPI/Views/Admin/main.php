5b46c997b462786eb273110c52bfcdd5336388fe
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-02 10:09:59 +0200

    Update main.php

--- src/Hyyan/WPI/Views/Admin/main.php
+++ src/Hyyan/WPI/Views/Admin/main.php
@@ -1,10 +1,16 @@
 <?php
+
+declare(strict_types=1);
+
 if (!defined('ABSPATH')) {
     exit('restricted access');
 }
 ?>
 
-<?php $vars['self']->show_navigation(); ?>
-<div class="postbox" style="padding: 10px;">
-    <?php $vars['self']->show_forms(); ?>
+<div class="wpi-admin-wrapper">
+    <?php $vars['self']->show_navigation(); ?>
+    
+    <div class="postbox wpi-settings-form">
+        <?php $vars['self']->show_forms(); ?>
+    </div>
 </div>

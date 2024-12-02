a4b3a897fd017aaa65a95dce477fe2007ba75e76
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-02 10:03:04 +0200

    Update admin.php

--- src/Hyyan/WPI/Views/admin.php
+++ src/Hyyan/WPI/Views/admin.php
@@ -1,34 +1,39 @@
 <?php
+
+declare(strict_types=1);
+
 if (!defined('ABSPATH')) {
     exit('restricted access');
 }
+
+use Hyyan\WPI\Plugin;
 ?>
+
 <div class="wrap">
-    <h2> <?php _e('WooPoly Advanced Options', 'woo-poly-integration'); ?></h2>
+    <h2><?php esc_html_e('WooPoly Advanced Options', 'woo-poly-integration'); ?></h2>
+    
     <div id="poststuff">
         <div id="post-body" class="metabox-holder columns-2">
-
             <!-- main content -->
             <div id="post-body-content">
-                <?php echo \Hyyan\WPI\Plugin::getView('Admin/main', $vars) ?>
+                <?php echo wp_kses_post(Plugin::getView('Admin/main', $vars)); ?>
             </div>
 
             <!-- sidebar -->
             <div id="postbox-container-1" class="postbox-container">
-
                 <!-- About the plugin -->
                 <div class="postbox">
-                    <?php echo \Hyyan\WPI\Plugin::getView('Admin/about') ?>
+                    <?php echo wp_kses_post(Plugin::getView('Admin/about')); ?>
                 </div>
 
                 <!-- Support plugin -->
                 <div class="postbox">
-                    <?php echo \Hyyan\WPI\Plugin::getView('Admin/support') ?>
+                    <?php echo wp_kses_post(Plugin::getView('Admin/support')); ?>
                 </div>
 
                 <!-- Need help -->
-                <div class = "postbox">
-                    <?php echo \Hyyan\WPI\Plugin::getView('Admin/getHelp') ?>
+                <div class="postbox">
+                    <?php echo wp_kses_post(Plugin::getView('Admin/getHelp')); ?>
                 </div>
             </div>
         </div>

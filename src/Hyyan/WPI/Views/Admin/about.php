17dad7c308afeb710b4e0efb6e9047b05bdb4c14
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-02 10:07:39 +0200

    Update about.php

--- src/Hyyan/WPI/Views/Admin/about.php
+++ src/Hyyan/WPI/Views/Admin/about.php
@@ -1,35 +1,45 @@
 <?php
+
+declare(strict_types=1);
+
 if (!defined('ABSPATH')) {
     exit('restricted access');
 }
+
+use Hyyan\WPI\Plugin;
 ?>
+
 <h3>
-    <span>
-        <?php _e('About The Plugin', 'woo-poly-integration'); ?>
-    </span>
+    <span><?php esc_html_e('About The Plugin', 'woo-poly-integration'); ?></span>
 </h3>
+
 <div class="inside">
     <div>
         <?php
-        _e('The plugin is an open source project
-            which aims to fill the gap between
-            <a href="https://wordpress.org/plugins/woocommerce/">WooCommerce</a>
-            and <a href="https://wordpress.org/plugins/polylang/">Polylang</a>', 'woo-poly-integration'
+        printf(
+            '%s. %s <a target="_blank" rel="noopener noreferrer" href="%s">%s</a>.',
+            sprintf(
+                esc_html__('The plugin is an open source project which aims to fill the gap between %1$s and %2$s', 'woo-poly-integration'),
+                '<a href="https://wordpress.org/plugins/woocommerce/">WooCommerce</a>',
+                '<a href="https://wordpress.org/plugins/polylang/">Polylang</a>'
+            ),
+            esc_html__('For more information please see:', 'woo-poly-integration'),
+            'https://github.com/hyyan/woo-poly-integration/wiki',
+            esc_html__('documentation pages', 'woo-poly-integration')
         );
-        echo('. ');
-        _e('For more information please see:', 'woo-poly-integration');
-        echo(' <a target="_blank" href="https://github.com/hyyan/woo-poly-integration/wiki">');
-        _e('documentation pages', 'woo-poly-integration');
-        echo('</a>.');
         ?>
-        <br><br>
 
-        <p>
-            <?php _e('Author : ', 'woo-poly-integration') ?>
-            <a href="https://github.com/hyyan">Hyyan Abo Fakher</a>
+        <p class="author-info">
+            <?php
+            printf(
+                '%s <a href="%s" rel="noopener noreferrer">%s</a>',
+                esc_html__('Author : ', 'woo-poly-integration'),
+                'https://github.com/hyyan',
+                'Hyyan Abo Fakher'
+            );
+            ?>
         </p>
 
-        <?php echo \Hyyan\WPI\Plugin::getView('badges'); ?>
-
+        <?php echo wp_kses_post(Plugin::getView('badges')); ?>
     </div>
 </div>

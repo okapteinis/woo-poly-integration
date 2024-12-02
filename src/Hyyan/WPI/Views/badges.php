66f22b7e93ecf622ee60aa600544f2dbd4af2d8d
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-02 10:04:08 +0200

    Update badges.php

--- src/Hyyan/WPI/Views/badges.php
+++ src/Hyyan/WPI/Views/badges.php
@@ -1,14 +1,22 @@
 <?php
+
+declare(strict_types=1);
+
 if (!defined('ABSPATH')) {
     exit('restricted access');
 }
 ?>
-<a href="https://wordpress.org/plugins/woo-poly-integration" target="_blank">
-    <img src="https://poser.pugx.org/hyyan/woo-poly-integration/v/stable.svg" />
-</a>
-<a href="https://github.com/hyyan/woo-poly-integration/blob/master/LICENSE" target="_blank">
-    <img src="https://poser.pugx.org/hyyan/woo-poly-integration/license.svg"/>
-</a>
-<a href="http://www.gitchecker.com/hyyan/woo-poly-integration" target="_blank">
-    <img src="http://www.repostatus.org/badges/latest/active.svg" />
-</a>
+
+<div class="plugin-badges">
+    <a href="https://wordpress.org/plugins/woo-poly-integration" target="_blank" rel="noopener noreferrer">
+        <img src="https://poser.pugx.org/hyyan/woo-poly-integration/v/stable.svg" alt="Version Badge" />
+    </a>
+    
+    <a href="https://github.com/hyyan/woo-poly-integration/blob/master/LICENSE" target="_blank" rel="noopener noreferrer">
+        <img src="https://poser.pugx.org/hyyan/woo-poly-integration/license.svg" alt="License Badge" />
+    </a>
+    
+    <a href="http://www.gitchecker.com/hyyan/woo-poly-integration" target="_blank" rel="noopener noreferrer">
+        <img src="http://www.repostatus.org/badges/latest/active.svg" alt="Project Status Badge" />
+    </a>
+</div>

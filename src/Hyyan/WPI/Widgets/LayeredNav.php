78cf4ca1f484ca8a5ac1d2ba17881cb1b1063f13
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-02 10:17:01 +0200

    Update LayeredNav.php

--- src/Hyyan/WPI/Widgets/LayeredNav.php
+++ src/Hyyan/WPI/Widgets/LayeredNav.php
@@ -1,42 +1,22 @@
 <?php
 
-/**
- * This file is part of the hyyan/woo-poly-integration plugin.
- * (c) Hyyan Abo Fakher <hyyanaf@gmail.com>.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+declare(strict_types=1);
 
 namespace Hyyan\WPI\Widgets;
 
-/**
- * LayeredNav.
- *
- * @author Hyyan Abo Fakher <hyyanaf@gmail.com>
- */
+use WC_Query;
+
 class LayeredNav
 {
-    /**
-     * Construct object.
-     */
     public function __construct()
     {
-        add_action('init', array($this, 'layeredNavInit'), 1000);
+        add_action('init', [$this, 'layeredNavInit'], 1000);
     }
 
-    /**
-     * Layered Nav Init.
-     *
-     * @global array $_chosen_attributes
-     *
-     * @return false if not layered nav filter
-     */
-    public function layeredNavInit()
+    public function layeredNavInit(): bool
     {
-        if (
-                !(is_active_widget(false, false, 'woocommerce_layered_nav', true) &&
-                !is_admin())
+        if (!is_active_widget(false, false, 'woocommerce_layered_nav', true) || 
+            is_admin()
         ) {
             return false;
         }
@@ -47,22 +27,31 @@ class LayeredNav
         foreach ($attributes as $tax) {
             $attribute = wc_sanitize_taxonomy_name($tax->attribute_name);
             $taxonomy = wc_attribute_taxonomy_name($attribute);
-            $name = 'filter_'.$attribute;
+            $name = 'filter_' . $attribute;
 
-            if (!(!empty($_GET[$name]) && taxonomy_exists($taxonomy))) {
+            if (empty($_GET[$name]) || !taxonomy_exists($taxonomy)) {
                 continue;
             }
 
-            $terms = explode(',', $_GET[$name]);
-            $termsTranslations = array();
-
-            foreach ($terms as $ID) {
-                $translation = pll_get_term($ID);
-                $termsTranslations [] = $translation ? $translation : $ID;
-            }
+            $terms = explode(',', sanitize_text_field($_GET[$name]));
+            $termsTranslations = $this->getTermTranslations($terms);
 
             $_GET[$name] = implode(',', $termsTranslations);
             $_chosen_attributes[$taxonomy]['terms'] = $termsTranslations;
         }
+
+        return true;
+    }
+
+    private function getTermTranslations(array $terms): array
+    {
+        $termsTranslations = [];
+        
+        foreach ($terms as $ID) {
+            $translation = pll_get_term($ID);
+            $termsTranslations[] = $translation ?: $ID;
+        }
+
+        return $termsTranslations;
     }
 }

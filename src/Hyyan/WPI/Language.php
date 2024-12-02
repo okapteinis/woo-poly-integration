803722a3d22958d513d048e3c260437a4f4d0d44
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-02 18:04:17 +0200

    Update Language.php

--- src/Hyyan/WPI/Language.php
+++ src/Hyyan/WPI/Language.php
@@ -1,29 +1,65 @@
-public function downloadWhenPolylangAddLanguage(): bool
+<?php
+
+declare(strict_types=1);
+
+namespace Hyyan\WPI;
+
+use RuntimeException;
+use Hyyan\WPI\Admin\Settings;
+use Hyyan\WPI\Admin\Features;
+
+class Language
 {
-    if ('off' === Settings::getOption('language-downloader', Features::getID(), 'on')) {
-        return false;
+    public function __construct()
+    {
+        add_action('wp_loaded', [$this, 'downloadWhenPolylangAddLanguage']);
     }
 
-    if (!isset($_REQUEST['pll_action']) ||
-        'add' !== esc_attr($_REQUEST['pll_action'] ?? '')
-    ) {
-        return false;
+    public function downloadWhenPolylangAddLanguage(): bool
+    {
+        if ('off' === Settings::getOption('language-downloader', Features::getID(), 'on')) {
+            return false;
+        }
+
+        if (!isset($_REQUEST['pll_action']) ||
+            'add' !== esc_attr($_REQUEST['pll_action'] ?? '')
+        ) {
+            return false;
+        }
+
+        $name = esc_attr($_REQUEST['name'] ?? '');
+        $locale = esc_attr($_REQUEST['locale'] ?? '');
+        if ('en_us' === strtolower($locale)) {
+            return true;
+        }
+
+        try {
+            return TranslationsDownloader::download($locale, $name);
+        } catch (RuntimeException $ex) {
+            add_settings_error(
+                'general',
+                $ex->getCode(),
+                $ex->getMessage()
+            );
+            return false;
+        }
     }
 
-    $name = esc_attr($_REQUEST['name'] ?? '');
-    $locale = esc_attr($_REQUEST['locale'] ?? '');
-    if ('en_us' === strtolower($locale)) {
-        return true;
-    }
+    protected function getFields(): array
+    {
+        $fields = [
+            [
+                'name' => 'language-downloader',
+                'type' => 'checkbox',
+                'default' => 'on',
+                'label' => __('Translation Downloader', 'woo-poly-integration'),
+                'desc' => __(
+                    'Download Woocommerce translations when a new polylang language is added',
+                    'woo-poly-integration'
+                ),
+            ],
+        ];
 
-    try {
-        return TranslationsDownloader::download($locale, $name);
-    } catch (RuntimeException $ex) {
-        add_settings_error(
-            'general',
-            $ex->getCode(),
-            $ex->getMessage()
-        );
-        return false;
+        return $fields;
     }
 }

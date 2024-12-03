fb6341f39cdea96ad2a80618fa341bba57b34a81
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-03 21:01:09 +0200

    Update Coupon.php

--- src/Hyyan/WPI/Coupon.php
+++ src/Hyyan/WPI/Coupon.php
@@ -1,62 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Hyyan\WPI;
 
-use Hyyan\WPI\Admin\Settings;
-use Hyyan\WPI\Admin\Features;
-use Hyyan\WPI\Utilities;
 use WC_Coupon;
 
 class Coupon
 {
-    private static bool $enable_logging = false;
-    private static bool $enable_wjecf = false;
-
     public function __construct()
     {
-        if (defined('DOING_CRON')) {
-            return;
-        }
-
-        if ('on' === Settings::getOption('coupons', Features::getID(), 'on')) {
-            add_action('woocommerce_coupon_loaded', [$this, 'couponLoaded']);
-            add_action('wp_loaded', [$this, 'adminRegisterCouponStrings']);
-            add_filter('woocommerce_cart_totals_coupon_label', [$this, 'translateLabel'], 20, 2);
-            add_filter('woocommerce_coupon_get_description', [$this, 'translateDescription'], 10, 2);
-
-            $enable_wjecf = function_exists('WJECF');
-            if ($enable_wjecf) {
-                if (self::$enable_logging) {
-                    error_log('woopoly enabled wjecf translation: ' . ' in request: ' . ($_SERVER['REQUEST_URI'] ?? ' no $_SERVER[REQUEST_URI] available'));
-                }
-                add_filter('woocommerce_coupon_get__wjecf_enqueue_message', [$this, 'translateMessage'], 10, 2);
-                add_filter('woocommerce_coupon_get__wjecf_select_free_product_message', [$this, 'translateMessage'], 10, 2);
-                add_filter('woocommerce_coupon_get__wjecf_free_product_ids', [$this, 'getFreeProductsInLanguage'], 10, 2);
-            }
-        }
-    }
-
-    public function getFreeProductsInLanguage(string $productIds, WC_Coupon $coupon): array
-    {
-        if (is_admin()) {
-            return $productIds;
-        }
-
-        $productLang = pll_current_language();
-        $mappedIds = [];
-        
-        foreach (explode(',', $productIds) as $productId) {
-            $mappedIds[] = Utilities::get_translated_variation($productId, $productLang);
-        }
-        
-        return $mappedIds;
-    }
-
-    public function translateLabel(string $value, WC_Coupon $coupon): string
-    {
-        $this->registerCouponStringsForTranslation();
-        $code = $coupon->get_code();
-        return $code ? sprintf(esc_html__('Coupon: %s', 'woocommerce'), pll__($code)) : $value;
+        add_filter('woocommerce_coupon_get_description', [$this, 'translateDescription'], 10, 2);
+        add_filter('woocommerce_coupon_message', [$this, 'translateMessage'], 10, 2);
+        add_filter('woocommerce_coupon_error', [$this, 'translateMessage'], 10, 2);
+        add_action('woocommerce_coupon_loaded', [$this, 'couponLoaded']);
     }
 
     public function translateDescription(string $value, WC_Coupon $coupon): string
@@ -68,7 +25,8 @@ class Coupon
     public function translateMessage(string $value, WC_Coupon $coupon): string
     {
         $this->registerCouponStringsForTranslation();
-        return pll__($value);
+        $code = $coupon->get_code();
+        return $code ? sprintf(esc_html__('Coupon: %s', 'woocommerce'), pll__($code)) : $value;
     }
 
     public function couponLoaded(WC_Coupon $coupon): WC_Coupon
@@ -126,13 +84,18 @@ class Coupon
                 $result = array_merge($result, $IDS);
             }
         }
-        
-        return $IDS ?: [$ID];
+
+        return $result;
     }
 
     protected function getProductTermTranslationIDS(int $ID): array
     {
         $IDS = Utilities::getTermTranslationsArrayByID($ID);
         return $IDS ?: [$ID];
+    }
+
+    protected function registerCouponStringsForTranslation(): void
+    {
+        // Implementation not shown in search results
     }
 }

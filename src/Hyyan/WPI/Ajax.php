48f1108c0c24906331e0a93e7bc57dde7e1cbe78
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-03 20:37:41 +0200

    Update Ajax.php

--- src/Hyyan/WPI/Ajax.php
+++ src/Hyyan/WPI/Ajax.php
@@ -1,23 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Hyyan\WPI;
 
 class Ajax
 {
     public function __construct()
     {
-        add_filter('woocommerce_ajax_get_endpoint', array($this, 'filter_woocommerce_ajax_get_endpoint'), 10, 2);
+        add_filter('woocommerce_ajax_get_endpoint', [$this, 'getEndpoint'], 10, 2);
     }
 
-    /**
-     * @param string $url
-     * @param string $request
-     * @return string
-     */
-    public function filter_woocommerce_ajax_get_endpoint(string $url, string $request): string
+    public function getEndpoint(string $url, string $request): string
     {
         global $polylang;
-        $lang = ($polylang->curlang) ? $polylang->curlang : $polylang->pref_lang;
-        return parse_url($polylang->filters_links->links->get_home_url($lang), PHP_URL_PATH) . '?' . parse_url($url, PHP_URL_QUERY);
+
+        if (!$polylang) {
+            return $url;
+        }
+
+        $lang = isset($polylang->curlang) ? $polylang->curlang : $polylang->pref_lang;
+        return parse_url(
+            $polylang->filters_links->links->get_home_url($lang), 
+            PHP_URL_PATH
+        ) . '?' . parse_url($url, PHP_URL_QUERY);
     }
 }

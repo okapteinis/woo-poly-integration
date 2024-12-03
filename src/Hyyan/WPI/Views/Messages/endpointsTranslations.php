b86ea872c7e268715b21b92633e5020f7faef5eb
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-03 20:18:30 +0200

    Update endpointsTranslations.php

--- src/Hyyan/WPI/Views/Messages/endpointsTranslations.php
+++ src/Hyyan/WPI/Views/Messages/endpointsTranslations.php
@@ -7,20 +7,15 @@ if (!defined('ABSPATH')) {
 }
 
 use Hyyan\WPI\Endpoints;
-?>
 
-<?php
 echo wp_kses(
     sprintf(
-        /* translators: %1$s is the URL, %2$s is the translated "Translate" text */
-        esc_html__('You can translate woocommerce endpoints, email strings, shipping methods from polylang strings tab. <a target="_blank" href="%1$s">%2$s</a>', 'woo-poly-integration'),
-        esc_url(add_query_arg(
-            [
-                'page' => 'mlang_strings',
-                'group' => Endpoints::getPolylangStringSection(),
-            ],
-            admin_url('admin.php')
-        )),
+        '%s <a href="%s" target="_blank">%s</a>',
+        esc_html__('To translate endpoints labels please visit', 'woo-poly-integration'),
+        esc_url(add_query_arg([
+            'page' => 'mlang_strings',
+            'group' => Endpoints::getPolylangStringSection(),
+        ], admin_url('admin.php'))),
         esc_html__('Translate', 'woo-poly-integration')
     ),
     [
@@ -30,4 +25,3 @@ echo wp_kses(
         ]
     ]
 );
-?>

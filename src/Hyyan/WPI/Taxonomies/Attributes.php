a455733796acc0b28084de984744ccb3554dda35
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-03 18:43:00 +0200

    Update Attributes.php

--- src/Hyyan/WPI/Taxonomies/Attributes.php
+++ src/Hyyan/WPI/Taxonomies/Attributes.php
@@ -1,39 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Hyyan\WPI\Taxonomies;
 
 use Hyyan\WPI\Utilities;
-use WooCommerce;
-use Polylang;
 
-class Attributes implements TaxonomiesInterface
+class Attributes
 {
     public function __construct()
     {
-        add_action('init', [$this, 'manageAttrLablesTranslation'], 11, 2);
         add_filter('woocommerce_attribute_label', [$this, 'translateAttrLable']);
-        add_action('admin_print_scripts', [$this, 'addAttrsTranslateLinks'], 100);
-    }
-
-    public function manageAttrLablesTranslation(): bool
-    {
-        global $polylang, $woocommerce;
-
-        if (!$polylang || !$woocommerce) {
-            return false;
-        }
-
-        $section = __('WooCommerce Attributes', 'woo-poly-integration');
-        
-        foreach (wc_get_attribute_taxonomies() as $attr) {
-            pll_register_string(
-                $attr->attribute_label,
-                $attr->attribute_label,
-                $section
-            );
-        }
-
-        return true;
+        add_action('admin_print_scripts', [$this, 'addAttrsTranslateLinks']);
     }
 
     public function translateAttrLable(string $label): string
@@ -48,7 +26,7 @@ class Attributes implements TaxonomiesInterface
             return false;
         }
 
-        if (!isset($_GET['page']) || 
+        if (!isset($_GET['page']) ||
             esc_attr($_GET['page']) !== 'product_attributes'
         ) {
             return false;
@@ -56,26 +34,24 @@ class Attributes implements TaxonomiesInterface
 
         $stringTranslationURL = $this->getStringTranslationUrl();
         $this->outputTranslationScripts($stringTranslationURL);
-
         return true;
     }
 
     private function getStringTranslationUrl(): string
     {
-        return add_query_arg(
-            [
-                'page' => 'mlang_strings',
-                'group' => __('WooCommerce Attributes', 'woo-poly-integration'),
-            ],
-            admin_url('admin.php')
+        return add_query_arg([
+            'page' => 'mlang_strings',
+            'group' => __('WooCommerce Attributes', 'woo-poly-integration'),
+        ],
+        admin_url('admin.php')
         );
     }
 
     private function outputTranslationScripts(string $stringTranslationURL): void
     {
         $buttonCode = sprintf(
-            '$("<a href=\'%s\' class=\'button button-primary button-large\'>%s</a><br><br>")' .
-            '.insertBefore(".attributes-table");',
+            '$("%s")'
+            . '.insertBefore(".attributes-table");',
             $stringTranslationURL,
             __('Translate Attributes Labels', 'woo-poly-integration')
         );
@@ -85,11 +61,11 @@ class Attributes implements TaxonomiesInterface
                 var $this = $(this);
                 var attrName = $this.parent().find("strong a").text();
                 var attrTranslateUrl = "%s&s=" + attrName;
-                var attrTranslateHref = 
-                    "<span class=\'translate\'>" +
+                var attrTranslateHref =
+                    "" +
                     "| " +
-                    "<a href=\'" + attrTranslateUrl + "\'>%s</a>" +
-                    "</span>";
+                    "%s" +
+                    "";
                 $this.append(attrTranslateHref);
             });',
             $stringTranslationURL,

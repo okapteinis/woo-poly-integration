14259ff3b579c01f16f68137843732638f6efda4
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-03 18:28:09 +0200

    Update Product.php

--- src/Hyyan/WPI/Product/Product.php
+++ src/Hyyan/WPI/Product/Product.php
@@ -1,31 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Hyyan\WPI\Product;
 
 use Hyyan\WPI\Admin\Settings;
 use Hyyan\WPI\Admin\Features;
 use Hyyan\WPI\Utilities;
-use WC_Product;
 
 class Product
 {
     public function __construct()
     {
-        add_filter('pll_get_post_types', [$this, 'manageProductTranslation']);
-        add_filter('admin_init', [$this, 'syncPostParent']);
-
-        $translate_option = Settings::getOption('new-translation-defaults', Features::getID(), 0);
-        if ($translate_option) {
-            add_filter('default_title', [$this, 'wpi_editor_title']);
-            add_filter('default_content', [$this, 'wpi_editor_content']);
-            add_filter('default_excerpt', [$this, 'wpi_editor_excerpt']);
-        }
-
-        add_filter('woocommerce_product_get_upsell_ids', [$this, 'getUpsellsInLanguage'], 10, 2);
-        add_filter('woocommerce_product_get_cross_sell_ids', [$this, 'getCrosssellsInLanguage'], 10, 2);
-        add_filter('woocommerce_product_get_children', [$this, 'getChildrenInLanguage'], 10, 2);
-        add_action('wp_ajax_woocommerce_feature_product', [__CLASS__, 'sync_ajax_woocommerce_feature_product'], 5);
-
         $this->initializeComponents();
     }
 
@@ -35,7 +21,7 @@ class Product
         new Variable();
         new Duplicator();
 
-        if ('on' === Settings::getOption('stock', Features::getID(), 'on') && 
+        if ('on' === Settings::getOption('stock', Features::getID(), 'on') &&
             'yes' === get_option('woocommerce_manage_stock')
         ) {
             new Stock();
@@ -56,7 +42,6 @@ class Product
 
         $targetValue = !$product->get_featured();
         $product_translations = Utilities::getProductTranslationsArrayByObject($product);
-        
         foreach ($product_translations as $product_translation) {
             if ($product_translation !== $product->get_id()) {
                 $translation = wc_get_product($product_translation);
@@ -68,31 +53,29 @@ class Product
         }
     }
 
-    public function getChildrenInLanguage(array $relatedIds, WC_Product $product): array
+    public function getChildrenInLanguage(array $relatedIds, \WC_Product $product): array
     {
         return $this->getProductIdsInLanguage($relatedIds, $product);
     }
 
-    public function getUpsellsInLanguage(array $relatedIds, WC_Product $product): array
+    public function getUpsellsInLanguage(array $relatedIds, \WC_Product $product): array
     {
         return $this->getProductIdsInLanguage($relatedIds, $product);
     }
 
-    public function getCrosssellsInLanguage(array $relatedIds, WC_Product $product): array
+    public function getCrosssellsInLanguage(array $relatedIds, \WC_Product $product): array
     {
         return $this->getProductIdsInLanguage($relatedIds, $product);
     }
 
-    public function getProductIdsInLanguage(array $productIds, WC_Product $product): array
+    public function getProductIdsInLanguage(array $productIds, \WC_Product $product): array
     {
         $productLang = pll_get_post_language($product->get_id());
         $mappedIds = [];
-
         foreach ($productIds as $productId) {
             $correctLanguageId = pll_get_post($productId, $productLang);
             $mappedIds[] = $correctLanguageId ?: $productId;
         }
-
         return $mappedIds;
     }
 
@@ -100,14 +83,12 @@ class Product
     {
         $options = get_option('polylang');
         $postTypes = $options['post_types'] ?? [];
-
         foreach (['product', 'product_variation'] as $type) {
             if (!in_array($type, $postTypes, true)) {
                 $options['post_types'][] = $type;
                 update_option('polylang', $options);
             }
         }
-
         $types[] = 'product';
         return $types;
     }
@@ -116,7 +97,6 @@ class Product
     {
         $options = get_option('polylang');
         $sync = $options['sync'] ?? [];
-
         if (!in_array('post_parent', $sync, true)) {
             $options['sync'][] = 'post_parent';
             update_option('polylang', $options);
@@ -143,7 +123,6 @@ class Product
         if (!isset($_GET['from_post'])) {
             return $default;
         }
-
         $post = get_post($_GET['from_post']);
         return $post ? $post->$field : $default;
     }

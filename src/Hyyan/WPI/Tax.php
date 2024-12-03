1464d43d4a0abf585822f37ea82d2f1b68d5ed82
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-03 21:56:06 +0200

    Update Tax.php

--- src/Hyyan/WPI/Tax.php
+++ src/Hyyan/WPI/Tax.php
@@ -1,16 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Hyyan\WPI;
 
-use Hyyan\WPI\Utilities;
 use WC_Product;
 
 class Tax
 {
     public function __construct()
     {
-        add_filter('woocommerce_get_price_suffix', [$this, 'filterPriceSuffix'], 10, 4);
         $this->registerTaxStringsForTranslation();
+        add_filter('woocommerce_get_price_suffix', [$this, 'filterPriceSuffix'], 10, 4);
     }
 
     public function registerTaxStringsForTranslation(): void
@@ -54,8 +55,8 @@ class Tax
         string $price,
         int $qty
     ): string {
-        if (!wc_tax_enabled() || 
-            $instance->get_tax_status() !== 'taxable' || 
+        if (!wc_tax_enabled() ||
+            $instance->get_tax_status() !== 'taxable' ||
             !($suffix = get_option('woocommerce_price_display_suffix'))
         ) {
             return '';
@@ -63,7 +64,6 @@ class Tax
 
         $suffix = $this->translateTaxString($suffix);
         $price = $price ?: $instance->get_price();
-
         $replacements = [
             '{price_including_tax}' => wc_price(
                 wc_get_price_including_tax($instance, ['qty' => $qty, 'price' => $price])
@@ -76,10 +76,7 @@ class Tax
         return str_replace(
             array_keys($replacements),
             array_values($replacements),
-            sprintf(
-                ' <small class="woocommerce-price-suffix">%s</small>',
-                wp_kses_post($suffix)
-            )
+            sprintf(' %s', wp_kses_post($suffix))
         );
     }
 }

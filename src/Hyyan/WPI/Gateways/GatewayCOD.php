18c228ebd9d49c0cb09388898a26e093ef73a6f6
okapteinis <31920576+okapteinis@users.noreply.github.com>
2024-12-04 13:14:56 +0200

    Update GatewayCOD.php

--- src/Hyyan/WPI/Gateways/GatewayCOD.php
+++ src/Hyyan/WPI/Gateways/GatewayCOD.php
@@ -5,46 +5,28 @@ declare(strict_types=1);
 namespace Hyyan\WPI\Gateways;
 
 use Hyyan\WPI\Utilities;
-use WC_Order;
 
-class GatewayCOD
+class GatewayCOD extends \WC_Gateway_COD
 {
-    public function __construct()
-    {
-        add_action('woocommerce_thankyou_cod', [$this, 'thankyou_page']);
-        add_action('woocommerce_email_before_order_table', [$this, 'email_instructions'], 10, 3);
-    }
-
-    public function thankyou_page(int $order_id): void
+    public function thankyou_page(): void
     {
         if ($this->instructions) {
-            echo wpautop(
-                wptexturize(
-                    wp_kses_post(
-                        function_exists('pll__') 
-                            ? pll__($this->instructions) 
-                            : __($this->instructions, 'woocommerce')
-                    )
-                )
-            );
+            echo wpautop(wptexturize(wp_kses_post(
+                function_exists('pll__') ? 
+                pll__($this->instructions) : 
+                __($this->instructions, 'woocommerce')
+            )));
         }
     }
 
-    public function email_instructions(WC_Order $order, bool $sent_to_admin, bool $plain_text = false): void
+    public function email_instructions(\WC_Order $order, bool $sent_to_admin, bool $plain_text = false): void
     {
-        if (!$this->instructions ||
-            $sent_to_admin ||
-            'cod' !== Utilities::get_payment_method($order)
-        ) {
-            return;
+        if ($this->instructions && !$sent_to_admin && 'cod' === Utilities::get_payment_method($order)) {
+            echo wpautop(wptexturize(
+                function_exists('pll__') ? 
+                pll__($this->instructions) : 
+                __($this->instructions, 'woocommerce')
+            )) . PHP_EOL;
         }
-
-        echo wpautop(
-            wptexturize(
-                function_exists('pll__') 
-                    ? pll__($this->instructions) 
-                    : __($this->instructions, 'woocommerce')
-            )
-        ) . PHP_EOL;
     }
 }
